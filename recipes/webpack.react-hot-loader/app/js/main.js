dceaf7070e995d732c77b8dbf22c252910689bd1
yuskesh <vin.bleu@gmail.com>
2016-07-04 15:23:51 -0700

    fix a typo

--- recipes/webpack.react-hot-loader/app/js/main.js
+++ recipes/webpack.react-hot-loader/app/js/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 // It's important to not define HelloWorld component right in this file
 // because in that case it will do full page reload on change
 import HelloWorld from './HelloWorld.jsx';
 
-ReactDom.render(<HelloWorld />, document.getElementById('react-root'));
+ReactDOM.render(<HelloWorld />, document.getElementById('react-root'));
